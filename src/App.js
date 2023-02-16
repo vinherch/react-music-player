5bad147fe60f336e6978ceca2cb1344fd7365cd6
Vincenz Herzog <herzogv88@gmail.com>
2023-02-16 16:46:16 +0100

    Add app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
+//Styles
+import "./styles/app.scss";
+
+//Components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import Navigation from "./components/Navigation";
+
+//Music Data
+import data from "./utils/songs";
+
 function App() {
+  //States
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [libraryStatus, setLibraryStatus] = useState(false);
+
   return (
-    <div className="App">
-      <h1>Music Player</h1>
+    <div className={`App ${libraryStatus ? "ml-30" : ""}`}>
+      <Navigation libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong} isPlaying={isPlaying} />
+      <Player currentSong={currentSong} setCurrentSong={setCurrentSong} songs={songs} setSongs={setSongs} isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
+      <Library songs={songs} setSongs={setSongs} setCurrentSong={setCurrentSong} libraryStatus={libraryStatus} />
     </div>
   );
 }
